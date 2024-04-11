8a36a76cc4b9a1be03e44c6965f168fd93364980
Fabio Squizzato <fsquizzato@visualmedica.com>
2024-04-11 17:04:52 -0300

    End about

--- app/components/TextoAcerca.js
+++ app/components/TextoAcerca.js
@@ -1,10 +1,20 @@
 export default function TextoAcerca({titulo=false,lista=false,texto=false}){
 
+    let listItems;
+    if(lista){
+        listItems = lista.map(element => {
+            return <p key={element.nombre}>{element.nombre}: {element.descripcion} </p>
+        })
+    }
+    
+
     return <>
         <div className="textoAcerca">
             <p className="tituloAcerca">{titulo}</p>
-            {lista}
             <p>{texto}</p>
+            <div className="listado">
+                {listItems}
+            </div>
         </div>
     </>
 
